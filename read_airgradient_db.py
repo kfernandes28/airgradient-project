7aae0c587767d062d0e2c1b5ae4c868ab00604ff
Kellan Fernandes <kellanfernandes@Kellans-MacBook-Pro.local>
2026-08-19 19:52:23 +0100

    Add PostgreSQL sensor reading storage

---
+++ read_airgradient_db.py
@@ -0,0 +1,76 @@
+import requests 
+from datetime import datetime
+
+import psycopg
+import time
+
+url = "http://192.168.0.140/measures/current"
+
+
+for i in range(8):
+    # get + store one reading
+    response =requests.get(url)
+
+    data = response.json()
+
+    #print (data)
+
+    temperature = data["atmp"]
+    co2 = data["rco2"]
+    pm25 = data["pm02"]
+    humidity = data["rhum"]
+    voc = data["tvocIndex"]
+    nox = data["noxIndex"]
+    timestamp = datetime.now().isoformat()
+
+    reading = {
+        "timestamp": timestamp,
+        "temperature_c": temperature,
+        "co2_ppm": co2,
+        "pm25_ugm3": pm25,
+        "humidity_pct": humidity,
+        "voc_index": voc,
+        "nox_index": nox,
+    }
+
+    print (reading)
+
+    connection = psycopg.connect(
+        host="localhost",
+        port=5432,
+        dbname="airgradient",
+        user="kellanfernandes"
+    )
+
+    cursor = connection.cursor()
+
+
+    cursor.execute(
+        """
+        INSERT INTO airgradient_readings (
+            timestamp,
+            temperature_c,
+            co2_ppm,
+            pm25_ugm3,
+            humidity_pct,
+            voc_index,
+            nox_index
+        )
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """,
+        (
+            reading["timestamp"],
+            reading["temperature_c"],
+            reading["co2_ppm"],
+            reading["pm25_ugm3"],
+            reading["humidity_pct"],
+            reading["voc_index"],
+            reading["nox_index"],
+        )
+    )
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    time.sleep(120)
